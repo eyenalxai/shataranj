bf590fe9e5cd3d24ada67857432593e0c1ab8210
Sax Dhiut <mail@iyxeyl.me>
2024-07-03 20:41:51 +0400

    feat: add custom square styles to Chessboard component
    The Chessboard component has been updated to include custom styling for both dark and light squares. The new style provides a more controlled and consistent look for the chessboard across different themes and environments.

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,17 @@ export default function Home() {
 
 	return (
 		<div className={cn(["w-5/6", "sm:w-4/5", "md:w-3/4", "lg:w-2/3", "xl:w-1/3"])}>
-			<Chessboard position={chessboard.fen()} onPieceDrop={onPieceDrop} arePiecesDraggable={!disabled} />
+			<Chessboard
+				position={chessboard.fen()}
+				onPieceDrop={onPieceDrop}
+				arePiecesDraggable={!disabled}
+				customDarkSquareStyle={{
+					backgroundColor: "#404040"
+				}}
+				customLightSquareStyle={{
+					backgroundColor: "#737373"
+				}}
+			/>
 		</div>
 	)
 }
