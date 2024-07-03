f15d0540c9271161356bb0c3cfc27b19b0bd5bfb
Sax Dhiut <mail@iyxeyl.me>
2024-07-03 23:57:25 +0400

    feat: add custom chessboard component
    The newly added CustomChessboard component integrates the use of themes. This significantly improves the chessboard UI by allowing it to adjust its visual appearance based on the selected theme. The component has been appropriately integrated into the Home function of page.tsx.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
+import { CustomChessboard } from "@/components/custom-chessboard"
 import { SelectStrategies } from "@/components/strategy/select-strategies"
 import { useChessGame } from "@/lib/hooks/chess-game"
 import { cn } from "@/lib/utils"
-import { Chessboard } from "react-chessboard"
 
 export default function Home() {
 	const { chessboard, onPieceDrop, disabled, playerControls, setPlayerStrategy } = useChessGame()
@@ -14,17 +14,7 @@ export default function Home() {
 			<div
 				className={cn(["size-[320px]", "sm:size-[360pcx]", "md:size-[480px]", "lg:size-[560px]", "xl:size-[600px]"])}
 			>
-				<Chessboard
-					position={chessboard.fen()}
-					onPieceDrop={onPieceDrop}
-					arePiecesDraggable={!disabled}
-					customDarkSquareStyle={{
-						backgroundColor: "#404040"
-					}}
-					customLightSquareStyle={{
-						backgroundColor: "#737373"
-					}}
-				/>
+				<CustomChessboard fen={chessboard.fen()} onPieceDrop={onPieceDrop} disabled={disabled} />
 			</div>
 		</div>
 	)
---
+++ components/custom-chessboard.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useTheme } from "next-themes"
+
+import type { Square } from "chess.js"
+import { Chessboard } from "react-chessboard"
+
+type CustomChessboardProps = {
+	fen: string
+	onPieceDrop: (sourceSquare: Square, targetSquare: Square) => boolean
+	disabled: boolean
+}
+
+export const CustomChessboard = ({ fen, onPieceDrop, disabled }: CustomChessboardProps) => {
+	const { resolvedTheme } = useTheme()
+
+	return (
+		<Chessboard
+			position={fen}
+			onPieceDrop={onPieceDrop}
+			arePiecesDraggable={!disabled}
+			customDarkSquareStyle={{
+				backgroundColor: resolvedTheme === "dark" ? "#404040" : "#737373"
+			}}
+			customLightSquareStyle={{
+				backgroundColor: resolvedTheme === "dark" ? "#737373" : "#d4d4d4"
+			}}
+		/>
+	)
+}
