342dc838b8dd18d6e9096cd7551aca7c78ac0af3
Sax Dhiut <mail@iyxeyl.me>
2024-07-04 17:34:15 +0400

    feat: add latin subset to fontMono
    This commit adds a new subset, "latin", to the fontMono variable. This addition will allow the typography to support characters and glyphs from the latin script, improving the language coverage of the application.

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -2,5 +2,6 @@ import { Ubuntu_Mono as FontMono } from "next/font/google"
 
 export const fontMono = FontMono({
 	weight: ["400", "700"],
-	variable: "--font-mono"
+	variable: "--font-mono",
+	subsets: ["latin"]
 })
